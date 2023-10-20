0b0fd6808bd2ca20b028c356c5ff17b44abf68a5
lolipo2k <andreasyagin@yandex.ru>
2023-10-20 22:03:58 +0300

    fix home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,17 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\Content;
+use App\Models\Report;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        $list = new Event();
-        $list = $list::where('status', 1)->limit(10)->get();
+        $baners_event = Event::where('onmain', 1)->get();
+        $baners_report = Report::where('onmain', 1)->get();
+        $baners_content = Content::where('onmain', 1)->get();
 
-        $content = new Content();
-        $content = $content::where('status', 1)->orderByDesc('id')->limit(10)->get();
-
-        return view("home", compact('list', 'content'));
+        return view("home", compact('baners_event', 'baners_report', 'baners_content'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,30 @@
 @section('content')
 <div class="col-12 col-lg-5">
     <div class="article-wrap">
-        <h4>
-            Последние мероприятия:
-        </h4>
-        @foreach($list as $item)
+        @foreach($baners_event as $item)
         <article>
             <div class="article-title">
                 <a href="/event/{{$item->id}}">
+                    <b>{{ date('d-m-y', strtotime($item->created_at)) }}</b>
+                    {{$item->title_ru}}
+                </a>
+            </div>
+        </article>
+        @endforeach
+        @foreach($baners_report as $item)
+        <article>
+            <div class="article-title">
+                <a href="/report/{{$item->id}}">
+                    <b>{{ date('d-m-y', strtotime($item->created_at)) }}</b>
+                    {{$item->title_ru}}
+                </a>
+            </div>
+        </article>
+        @endforeach
+        @foreach($baners_content as $item)
+        <article>
+            <div class="article-title">
+                <a href="/content/{{$item->id}}">
                     <b>{{ date('d-m-y', strtotime($item->created_at)) }}</b>
                     {{$item->title_ru}}
                 </a>
@@ -21,23 +38,6 @@
                 Все материалы
             </a>
         </div>
-    </div>
-
-
-    <div class="article-wrap">
-        <h4>
-            Последние статьи:
-        </h4>
-        @foreach($content as $item)
-        <article>
-            <div class="article-title">
-                <a href="/content/{{$item->id}}">
-                    <b>{{ date('d-m-y', strtotime($item->created_at)) }}</b>
-                    {{$item->title_ru}}
-                </a>
-            </div>
-        </article>
-        @endforeach
     </div>
 </div>
 @stop
