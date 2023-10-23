11d7ad6db27347e5b6f76f634ce1668e8792ba85
lolipo2k <andreasyagin@yandex.ru>
2023-10-23 22:59:56 +0300

    dump user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,6 +16,7 @@ class UsersController extends Controller
     public function single(Request $request)
     {
         $item = User::find($request->id);
+        dd($item);
         return view("userSingle", compact('item'));
     }
 }
