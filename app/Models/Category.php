9a2354871599a8478f02b8dc6f67d6c31957a8f7
lolipo2k <andreasyagin@yandex.ru>
2023-10-28 00:03:50 +0300

    fix max hits

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -17,6 +17,7 @@ class Category extends Model
     {
         $max = Category::orderByDesc('hits')->first();
         $this->hits = ($this->hits != null && $this->hits != 0) ? $this->hits : 1;
+        $max->hits = ($max->hits == 0) ? 1 : $max->hits;
         $px = ($this->hits * 15) / $max->hits + 15;
         return round($px, 2);
     }
