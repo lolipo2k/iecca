37f5fc43878f70440a4640962296ae8784fa3d33
lolipo2k <andreasyagin@yandex.ru>
2023-10-29 15:10:07 +0300

    edit title

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,7 +10,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>Home</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}">
+    <title>Iecca</title>
 </head>
 
 <body>
